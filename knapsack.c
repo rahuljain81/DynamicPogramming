8106d2286161d8c592f1c49427bbd69d00bcb499
Rahul Jain <rahuljain81@gmail.com>
2016-05-12 22:15:04 +0530

    knapsack

--- knapsack.c
+++ knapsack.c
@@ -1,3 +1,12 @@
+/*
+   The knapsack problem or rucksack problem is a problem in combinatorial optimization: 
+   Given a set of items, each with a weight and a value,
+   determine the number of each item to include in a collection 
+   so that the total weight is less than or equal to a given limit 
+   and the total value is as large as possible.
+   */
+
+
 #include <stdio.h>
 #define max(a,b) (a > b ? a : b)
 
@@ -7,19 +16,19 @@
 int knapsackSize = 10;
 int nItems = MAX_INDEX;
 int matrix[MAX][MAX] = {0};
-int weights[MAX_INDEX] = {5,4,6,3};
-int values[MAX_INDEX] = {10,40,30,50};
+int weights[] = {5,4,6,3};
+int values[] = {10,40,30,50};
 
 void dump()
 {
 	int i, j;
-	printf ("Wei->\t");
+	printf ("Wei->\t\t");
 	for (j=1; j < knapsackSize+1; j++)
 		printf ("%d\t", j);
 	printf ("\n");
 	for (i=0; i < MAX_INDEX; i++)
 	{
-		printf ("%d(%d):\t", values[i], weights[i]);
+		printf ("(v%d,w%d):\t", values[i], weights[i]);
 		for (j=1; j < knapsackSize+1; j++)
 			printf ("%d\t", matrix[i][j]);
 		printf ("\n");
@@ -30,6 +39,7 @@ void dump()
 int knapsack(int index, int size)
 {
     int take,dontTake;
+	printf ("[%d,%d]\n", index, size);
 
     take = dontTake = 0;
 
@@ -44,11 +54,13 @@ int knapsack(int index, int size)
         if (weights[index] <= size)
 		{
             matrix[index][size] = values[index];
+			printf ("[%d,%d]=[%d]\n", index, size, matrix[index][size]);
             return values[index];
         }
         else
 		{
             matrix[index][size] = 0;
+			printf ("[%d,%d]=[%d]\n", index, size, matrix[index][size]);
             return 0;
         }
     }
@@ -60,8 +72,10 @@ int knapsack(int index, int size)
 	//Excluding the index
     dontTake = knapsack(index-1, size);
 
+	printf ("[%d,%d] %d,%d\n", index, size, take, dontTake);
     matrix[index][size] = max (take, dontTake);
-	dump();
+	printf ("[%d,%d]=[%d]\n", index, size, matrix[index][size]);
+	//dump();
 
     return matrix[index][size];
 
@@ -70,8 +84,9 @@ int knapsack(int index, int size)
 int main()
 {
 
-    printf("Max value = %d\n",knapsack(nItems-1,knapsackSize));
+    knapsack(nItems-1,knapsackSize);
 	dump();
+    printf("Max value = %d\n",matrix[MAX_INDEX-1][knapsackSize]);
 
     return 0;
 }
